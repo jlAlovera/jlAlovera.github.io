d7c3ba9e3c09956706d9984ad93ff1a288e4e835
jlAlovera <rencealovera@gmail.com>
2025-01-31 12:13:33 +0800

    Added images and edit codes and descriptions in html and JS

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const message = document.getElementById('message').value;
-
-    alert(`Thank you, ${name}! Your message has been sent.`);
-    
-    document.getElementById('contact-form').reset();
-});
-
 const elements = document.querySelectorAll('.project, .assignment, .award, .certification');
 
 const observer = new IntersectionObserver((entries) => {
